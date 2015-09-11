f191ccf59ff397ed872e118c8c19073e9a792020
Stephen Elaschuk <selaschuk@users.noreply.github.com>
2015-09-11 13:23:55 -0600

    Update google-export.sql
    Changed to only pull the year specified in the 'CURRENT_YEAR' variable, as it was causing issues with multiple sections with the same section numbers from year to year.
    
    Powerschool defines "Current Year" as the last two digits of the year of the start of the school year + 10 (ie. 2015-2016 = 15 + 10 = current year 25)

--- google-export.sql
+++ google-export.sql
@@ -12,100 +12,75 @@ SET VERIFY OFF
 set recsep off
 alter session set nls_date_format = 'mm/dd/yyyy';
 
-spool C:\Exports\Google\schools.csv
+define CURRENT_YEAR = 25
+
+spool E:\exports\Google\schools.csv
 PROMPT school_id,school_name
 select
-CASE
-WHEN Alternate_School_Number IS NULL THEN TO_CHAR(School_Number)
-WHEN Alternate_School_Number = '0' THEN TO_CHAR(School_Number)
-ELSE TO_CHAR(Alternate_School_Number)
-END||','||
+School_Number||','||
 Abbreviation
 FROM Schools
-WHERE School_Number != '999999';
 spool off
 
-spool C:\Exports\Google\students.csv
+spool E:\exports\Google\students.csv
 PROMPT school_id,student_id,first_name,last_name,grade_level,user_name
 select
-CASE
-WHEN Schools.Alternate_School_Number IS NULL THEN TO_CHAR(Schools.School_Number)
-WHEN Schools.Alternate_School_Number = '0' THEN TO_CHAR(Schools.School_Number)
-ELSE TO_CHAR(Schools.Alternate_School_Number)
-END||','||
+Schools.School_Number||','||
 Students.ID||','||
-CASE
-WHEN ps_customfields.getstudentscf(Students.id, 'AB_PreferredNameInd') = 'Alias' THEN ps_customfields.getstudentscf(Students.id, 'AB_AKA_Given_Names')
-ELSE Students.First_Name
-END||','||
-CASE
-WHEN ps_customfields.getstudentscf(Students.id, 'AB_PreferredNameInd') = 'Alias' THEN ps_customfields.getstudentscf(Students.id, 'AB_AKA_Surname')
-ELSE Students.Last_Name
-END||','||
+Students.First_Name||','||
+Students.Last_Name||','||
 CASE
 WHEN Students.Grade_Level = '0' THEN 'K'
-WHEN Students.Grade_Level < '0' THEN 'ECS'
+WHEN Students.Grade_Level < '0' THEN 'ECS' --Enter in your abbreviation for Pre-K instead of ECS
 ELSE TO_CHAR(Students.Grade_Level)
 END||','||
 Students.Student_Web_ID
 FROM Students
 JOIN Schools ON Students.SchoolID = Schools.School_Number
-WHERE Students.SchoolID NOT IN ('999999','0')
-AND Students.Enroll_Status = '0'
+WHERE Students.Enroll_Status = '0'
 AND Students.Student_Web_ID IS NOT NULL;
 spool off
 
-spool C:\Exports\Google\staff.csv
+spool E:\exports\Google\staff.csv
 PROMPT school_id,staff_id,first_name,last_name,user_name
 select
-CASE
-WHEN Schools.Alternate_School_Number IS NULL THEN TO_CHAR(Schools.School_Number)
-WHEN Schools.Alternate_School_Number = '0' THEN TO_CHAR(Schools.School_Number)
-ELSE TO_CHAR(Schools.Alternate_School_Number)
-END||','||
+Schools.School_Number||','||
 Teachers.ID||','||
 Teachers.First_Name||','||
 Teachers.Last_Name||','||
 Teachers.TeacherLoginID
 FROM Teachers
 JOIN Schools ON Teachers.SchoolID = Schools.School_Number
-WHERE SchoolID NOT IN ('999999','0')
-AND Status = '1'
+WHERE Status = '1'
 AND Teachers.TeacherLoginID IS NOT NULL
 AND Teachers.HomeSchoolID = Teachers.SchoolID;
 spool off
 
-spool C:\Exports\Google\classes.csv
+spool E:\exports\Google\classes.csv
 PROMPT school_id,class_id,course_name,section_name,staff_id
 select
-CASE
-WHEN Schools.Alternate_School_Number IS NULL THEN TO_CHAR(Schools.School_Number)
-WHEN Schools.Alternate_School_Number = '0' THEN TO_CHAR(Schools.School_Number)
-ELSE TO_CHAR(Schools.Alternate_School_Number)
-END||','||
+Schools.School_Number||','||
 Sections.ID||','||
 Courses.Course_Number||','||
 Sections.Section_Number||','||
 Sections.Teacher
 FROM Sections
 JOIN Courses ON Sections.Course_Number = Courses.Course_Number
-JOIN Schools ON Sections.SchoolID = Schools.School_Number;
+JOIN Schools ON Sections.SchoolID = Schools.School_Number
+WHERE SUBSTR(Sections.TermID, 1, 2) = &CURRENT_YEAR;
 spool off
 
-spool C:\Exports\Google\rosters.csv
+spool E:\exports\Google\rosters.csv
 PROMPT school_id,class_id,student_id
 select
-CASE
-WHEN Schools.Alternate_School_Number IS NULL THEN TO_CHAR(Schools.School_Number)
-WHEN Schools.Alternate_School_Number = '0' THEN TO_CHAR(Schools.School_Number)
-ELSE TO_CHAR(Schools.Alternate_School_Number)
-END||','||
+Schools.School_Number||','||
 SectionID||','||
 StudentID
 FROM CC
 JOIN Schools ON CC.SchoolID = Schools.School_Number
+JOIN Students ON CC.StudentID = Students.ID
 WHERE SectionID > 0
-AND TermID > 0;
+AND SUBSTR(TermID, 1, 2) = &CURRENT_YEAR;
 spool off
 
 exit;
